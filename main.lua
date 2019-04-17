6e5453ec25a133720bc0105530a5c59feb9dbe35
Malcolm <malcolm.maclure@gmail.com>
2019-04-16 21:23:17 -0400

    renderer tool

---
+++ main.lua
@@ -0,0 +1,21 @@
+local Renderer = require "tools/renderer"
+
+renderer = Renderer:create()
+
+function love.load()
+
+end
+
+
+
+function love.update(dt)
+
+end
+
+
+
+function love.draw()
+  renderer:draw()
+end
+
+
